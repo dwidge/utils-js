967d6f3e4007973b3125e44484d3253cd3cc7527
dwidge <dwidge+gh@gmail.com>
2024-09-13 07:11:55 +0200

    Add randInt randItem

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * from "./assert.js";
 export * from "./formatDate.js";
 export * from "./notNull.js";
 export * from "./prioritySelect.js";
+export * from "./randInt.js";
+export * from "./randItem.js";
 export * from "./unixSeconds.js";
---
+++ src/randInt.ts
@@ -0,0 +1,5 @@
+// Copyright DWJ 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+export const randInt = (max = 2 ** 32) => (Math.random() * max) | 0;
